d6ad3e738921e85f5e9a8025120e79de63bde135
Athanasios Alexandris <thanosalexander@gmail.com>
2018-10-13 09:35:26 +0300

    test

--- src/formGroup.vue
+++ src/formGroup.vue
@@ -1,12 +1,6 @@
 <template>
 	<div class="form-group" :class="getFieldRowClasses(field)">
-		<label v-if="fieldTypeHasLabel(field)" :for="getFieldID(field)" :class="field.labelClasses">
-			<span v-html="field.label"></span>
-			<span v-if='field.help' class="help">
-				<i class="icon"></i>
-				<div class="helpText" v-html='field.help'></div>
-			</span>
-		</label>
+	
 
 		<div class="field-wrap">
 			<component ref="child" :is="getFieldType(field)" :vfg="vfg" :disabled="fieldDisabled(field)" :model="model" :schema="field" :formOptions="options" @model-updated="onModelUpdated" @validated="onFieldValidated"></component>
@@ -14,6 +8,16 @@
 				<button v-for="(btn, index) in field.buttons" @click="buttonClickHandler(btn, field, $event)" :class="btn.classes" :key="index" v-text="btn.label"></button>
 			</div>
 		</div>
+
+			<label v-if="fieldTypeHasLabel(field)" :for="getFieldID(field)" :class="field.labelClasses">
+			<span v-html="field.label"></span>
+			<span v-if='field.help' class="help">
+				<i class="icon"></i>
+				<div class="helpText" v-html='field.help'></div>
+			</span>
+		</label>
+
+		testt
 
 		<div v-if="field.hint" class="hint" v-html="fieldHint(field)"></div>
 
