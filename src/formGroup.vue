c498aae74798ec728ba4eb97b76a5e0fa4b4e40f
Athanasios Alexandris <thanosalexander@gmail.com>
2018-10-13 12:05:52 +0300

    test

--- src/formGroup.vue
+++ src/formGroup.vue
@@ -4,24 +4,20 @@
 
 		<div class="field-wrap">
 			<component ref="child" :is="getFieldType(field)" :vfg="vfg" :disabled="fieldDisabled(field)" :model="model" :schema="field" :formOptions="options" @model-updated="onModelUpdated" @validated="onFieldValidated"></component>
-			
-			<label v-if="fieldTypeHasLabel(field)" :for="getFieldID(field)" :class="field.labelClasses">
-			<span v-html="field.label"></span>
-				<span v-if='field.help' class="help">
-					<i class="icon"></i>
-					<div class="helpText" v-html='field.help'></div>
-				</span>
-			</label>
-
 			<div v-if="buttonVisibility(field)" class="buttons">
 				<button v-for="(btn, index) in field.buttons" @click="buttonClickHandler(btn, field, $event)" :class="btn.classes" :key="index" v-text="btn.label"></button>
 			</div>
 		</div>
 
-			
+			<label v-if="fieldTypeHasLabel(field)" :for="getFieldID(field)" :class="field.labelClasses">
+			<span v-html="field.label"></span>
+			<span v-if='field.help' class="help">
+				<i class="icon"></i>
+				<div class="helpText" v-html='field.help'></div>
+			</span>
+		</label>
 
-	
-
+		
 
 		<div v-if="field.hint" class="hint" v-html="fieldHint(field)"></div>
 
