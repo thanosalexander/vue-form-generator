c921d57c2476f93c1ca9b0b8bf04d1663252581d
Athanasios Alexandris <thanosalexander@gmail.com>
2018-10-16 23:01:14 +0300

    test

--- src/fields/core/fieldInput.vue
+++ src/fields/core/fieldInput.vue
@@ -39,7 +39,7 @@
 		:width="schema.width"
 		:files="schema.files"
 		v-attributes="'input'"
-		:v-validate="schema.validations"
+		:v-validate="schema.validations.data"
 	/>
 
 	
@@ -57,7 +57,7 @@
 		{{value}}
 	</span>
 
-	<div v-show="errors.has('login-form.email')" class="help-block invalid-feedback">{{ errors.first('login-form.email') }}</div>
+	<div v-show="errors.has(schema.validations.data)" class="help-block invalid-feedback">{{ errors.first(schema.validations.data) }}</div>
 
 </div>	
 	
